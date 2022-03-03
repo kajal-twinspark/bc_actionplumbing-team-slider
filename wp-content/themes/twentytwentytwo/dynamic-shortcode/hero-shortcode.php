06bdfc87932671900dc4446c417cc43052bd34a7
unknown <kajal.jatav@twinspark.co>
2022-03-03 20:33:16 +0530

    Issue Resolved dyanamic shortcode.

--- wp-content/themes/twentytwentytwo/dynamic-shortcode/hero-shortcode.php
+++ wp-content/themes/twentytwentytwo/dynamic-shortcode/hero-shortcode.php
@@ -1,25 +1,6 @@
 <?php
-//Admin menu 
-add_action('admin_menu', 'bc_actionPlumbing_admin_menu');
 
-function bc_actionPlumbing_admin_menu() {
-    $parent_slug = 'bc_actionPlumbing';
-    add_menu_page(__('Banner Settings', 'twentytwentytwo'), __('Banner Settings', 'twentytwentytwo'), 'manage_options', $parent_slug, 'bc_actionPlumbing_banner_settings');
-}
 
-function bc_actionPlumbing_banner_settings() {
-    include_once 'templates/banner-menu-settings.php';
-}
-//Save banner settings
-add_action('admin_post_save_bc_actionplumbing_banner_settings', 'save_bc_actionplumbing_banner_settings');
-
-function save_bc_actionplumbing_banner_settings() {
-    if (isset($_POST['bc_actionPlumbing_banner_settings'])) {
-        update_option('bc_actionPlumbing_banner_settings', $_POST['bc_actionPlumbing_banner_settings']);
-        wp_redirect(admin_url('admin.php?page=bc_actionPlumbing'));
-        exit;
-    }
-}
 //Admin Enqueue scripts
 add_action('admin_enqueue_scripts', 'bc_actionPlumbing_admin_assets');
 
@@ -37,34 +18,27 @@ function bc_actionPlumbing_admin_assets($hook) {
 //Add Shortcode
 add_shortcode('bc_actionPlumbing_banner', 'bc_actionPlumbing_banner_shortcode');
 
-function bc_actionPlumbing_banner_shortcode() {
-    $banner_settings = get_option('bc_actionPlumbing_banner_settings');
-    if (!empty($banner_settings)) {
-        $title = $banner_settings['title'];
-        $content = $banner_settings['content'];
-        $btn_text = $banner_settings['button_text'];
-        $image_url = $banner_settings['image_url'];
-        $btn_url = $banner_settings['button_url'];
-    } else {
-        $title = 'Lorem Ipsum';
-        $content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quam
-              perspiciatis facilis beatae laudantium quidem enim sit sequi!";
-        $btn_text = 'Submit';
-        $btn_url = '#';
-        $image_url = get_template_directory_uri() . '/assets/images/bird-on-salmon.jpg';
-    }
-    $html = <<<html
-     <section id="home" class="section-showcase">
-        <article class="banner shoe-white shoe-left spacing" style="background-image: url('$image_url')">
-            <h3 class="banner-title">$title</h3>
+function bc_actionPlumbing_banner_shortcode($attr) {
+    $attr = shortcode_atts(
+            array(
+                'title'=>'',
+                'content'=>'',
+                'button_text'=>'',
+                'image_url'=>''
+            )
+            , $attr);
+    $image_url = $attr['image_url'];
+    $btn_url = $attr['button_url'];
+    $btn_text = $attr['button_text'];
+    $html = '<section id="home" class="section-showcase">
+        <article class="banner shoe-white shoe-left spacing" style="background-image: url('.$image_url.')">
+            <h3 class="banner-title">'.$attr['title'].'</h3>
             <p class="banner__description">
-              $content
+              '.$attr["content"].'
             </p>
-            <a href="$btn_url" class="btn">$btn_text</a>
+            <a href="'.$btn_url.'" class="btn">'.$btn_text.'</a>
           </article>
-    </section>
-
-html;
+    </section>';
     return $html;
 }
 // Frontend Script
@@ -76,17 +50,3 @@ function enqueue_bc_action_plumbing_banner() {
         wp_enqueue_style('bc-plumbing-banner', get_template_directory_uri() . '/assets/banner/banner.css', array('bootstrap-4'));
     }
 }
-
-//
-//add_action('admin_init', function () {
-//    $banner_content = array(
-//        'title' => 'Lorem Ipsum',
-//        'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-//        'button_text' => 'Submit',
-//        'image_url' => get_template_directory_uri() . '/assets/images/bird-on-salmon.jpg',
-//    );
-//    $get_banner_content = get_option('bc_actionPlumbing_banner_settings');
-//    if (!$get_banner_content) {
-//        add_option('bc_actionPlumbing_banner_settings', $banner_content);
-//    }
-//});

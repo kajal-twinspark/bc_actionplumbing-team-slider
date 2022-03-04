6f966adf6639e3f795e46725929861c53a95cfd9
unknown <kajal.jatav@twinspark.co>
2022-03-04 12:26:48 +0530

    Resolved accordion issue.

--- wp-content/themes/twentytwentytwo/assets/accordion/accordion .js
+++ wp-content/themes/twentytwentytwo/assets/accordion/accordion .js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
     $('.faq-heading').click(function () {
         $(this).parent('li').toggleClass('the-active').find('.faq-text').slideToggle();
+        $(this).parent('li').siblings().removeClass('the-active');
+        $(this).parent('li').siblings().find('.faq-text').hide();
     });
 });
